2159c0dbf9d5e657dd1931e90ae851342c0465d6
Andras Wirth <agrippa.wia@gmail.com>
2019-10-19 14:52:28 +0200

    Initialize

---
+++ src/main/java/hu/wirthandras/blok/ui/BlokUiApplication.java
@@ -0,0 +1,13 @@
+package hu.wirthandras.blok.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlokUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlokUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/wirthandras/blok/ui/BlokUiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.wirthandras.blok.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlokUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
